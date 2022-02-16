04e8eda50294d5aa4e5bd67c246fb9dc6e91ca06
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-02-16 14:30:19 -0600

    config routes

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source 'https://rubygems.org'
 
 ruby '2.7.2'
 
-gem "sinatra", "~> 2.2"
+gem 'sinatra', '~> 2.2'
--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+get '/items' do
+  'Get all items'
+end
+
+get '/items/:id' do
+  params[:id]
+end
+
+post '/items' do
+  "create items"
+end
