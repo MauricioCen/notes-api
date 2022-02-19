76524caf36c6ae7cfa491110ef3e59c7990df59c
Mauricio Cen <mauricio1998.cen@gmail.com>
2022-02-19 10:46:03 -0600

    add config server

--- app.rb
+++ app.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
-require 'sinatra'
+require 'sinatra/base'
 
-get '/' do
-  'Hello world!'
-end
+class App < Sinatra::Base
+  get '/' do
+    'Hello world!'
+  end
 
-get '/items' do
-  'Get all items'
-end
+  get '/items' do
+    'Get all items'
+  end
 
-get '/items/:id' do
-  params[:id]
-end
+  get '/items/:id' do
+    params[:id]
+  end
 
-post '/items' do
-  'create items'
-end
+  post '/items' do
+    'create items'
+  end
 
-put '/items/:id' do
-  params[:id]
-end
+  put '/items/:id' do
+    params[:id]
+  end
 
-delete '/items/:id' do
-  params[:id]
+  delete '/items/:id' do
+    params[:id]
+  end
 end
